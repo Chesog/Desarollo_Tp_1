d80bb98626cbce02f5c1ebc8635ed8cabeac30e4
Dario <otrantodario@gmail.com>
2023-04-17 22:13:52 -0300

    Add Enemy Skeleton Assets

---
+++ Assets/Scripts/Enemy/Enemy_Controller.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy_Controller : MonoBehaviour
+{
+    public float lookRad = 10f;
+    [SerializeField] private Transform target;
+    [SerializeField] NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        if (!agent)
+        {
+            Debug.LogError(message: $"{name}: (logError){nameof(agent)} is null");
+            enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        float distance  = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRad) 
+        {
+
+            transform.position = Vector3.MoveTowards(transform.position, target.position, 0.2f);
+            if (distance <= agent.stoppingDistance)
+            {
+                //Attack the Target
+                faceTarget();
+            }
+        }
+    }
+
+    private void faceTarget() 
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation,lookRotation,Time.deltaTime * 5f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position,lookRad);
+    }
+}
