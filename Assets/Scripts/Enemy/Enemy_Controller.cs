f4a3b82ae69b4b7156ad3cab4a2f59ab2853f14e
Dario <otrantodario@gmail.com>
2023-04-18 16:39:58 -0300

    Add Enemy Follow IA

--- Assets/Scripts/Enemy/Enemy_Controller.cs
+++ Assets/Scripts/Enemy/Enemy_Controller.cs
@@ -5,50 +5,61 @@ using UnityEngine.AI;
 
 public class Enemy_Controller : MonoBehaviour
 {
-    [SerializeField]private float lookRad = 10f;
-    [SerializeField]private float stopDistance = 5f;
+    [SerializeField] private float lookRad = 20f;
+    [SerializeField] private float stopDistance = 5f;
+    [SerializeField] private float speed = 5f;
     [SerializeField] private Transform target;
-    [SerializeField] NavMeshAgent agent;
+    [SerializeField] private Rigidbody rb;
 
     private void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
-        if (!agent)
+        target ??= GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        if (!target)
         {
-            Debug.LogError(message: $"{name}: (logError){nameof(agent)} is null");
+            Debug.LogError(message: $"{name}: (logError){nameof(target)} is null");
+            enabled = false;
+        }
+
+        rb ??= GetComponent<Rigidbody>();
+        if (!rb)
+        {
+            Debug.LogError(message: $"{name}: (logError){nameof(rb)} is null");
             enabled = false;
         }
     }
 
     private void Update()
     {
-        float distance  = Vector3.Distance(target.position, transform.position);
+        float distance = Vector3.Distance(transform.position,target.position);
+        faceTarget();
 
         if (distance <= lookRad) 
         {
-
-            transform.position = Vector3.MoveTowards(transform.position, target.position, 0.2f);
-            Debug.Log("Stop Distance" + stopDistance);
+            //transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            rb.velocity = gameObject.transform.forward * speed;
             if (distance <= stopDistance)
             {
-                Debug.Log("Distance" + distance);
-                transform.position = transform.position;
+
+                rb.velocity = new Vector3(0f,0f,0f);
                 //Attack the Target
-                faceTarget();
             }
         }
     }
 
-    private void faceTarget() 
+    private void faceTarget()
     {
         Vector3 direction = (target.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
-        transform.rotation = Quaternion.Slerp(transform.rotation,lookRotation,Time.deltaTime * 5f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        transform.LookAt(target.position);
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position,lookRad);
+        Gizmos.DrawWireSphere(transform.position, lookRad);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, stopDistance);
     }
 }
