54ff19d47253f4bd95b672f5915f44791f7d19fe
Dario <otrantodario@gmail.com>
2023-05-16 19:53:24 -0300

    Changed Menu Song && Menu Axe

---
+++ Assets/Scripts/Main_Menu/Menu_Music_Manager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_Music_Manager : MonoBehaviour
+{
+    [SerializeField] AudioClip musicClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SoundManager.Instance.PlayMusic(musicClip);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
