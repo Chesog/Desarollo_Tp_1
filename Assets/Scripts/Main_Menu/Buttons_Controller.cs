db4bbe1b8f973958437d4f322a64e5b3d6f2fbf3
Dario <otrantodario@gmail.com>
2023-05-17 00:45:38 -0300

    Add Credits

--- Assets/Scripts/Main_Menu/Buttons_Controller.cs
+++ Assets/Scripts/Main_Menu/Buttons_Controller.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -6,17 +7,57 @@ using UnityEngine;
 public class Buttons_Controller : MonoBehaviour
 {
     [SerializeField] GameObject windowMaster;
+    [SerializeField] CinemachineVirtualCamera cam;
+
+    [SerializeField] Transform mainMenu;
+    [SerializeField] GameObject menu_Canvas;
+    [SerializeField] Transform options;
+    [SerializeField] GameObject options_Canvas;
+    [SerializeField] Transform credits;
+    [SerializeField] GameObject credits_Canvas;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        cam.Follow = mainMenu;
+        cam.LookAt = mainMenu;
+        menu_Canvas.active = true;
+        options_Canvas.active = false;
+        credits_Canvas.active = false;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public void LookOptions() 
+    {
+        menu_Canvas.active = false;
+        options_Canvas.active = true;
+        credits_Canvas.active = false;
+        cam.Follow = options;
+        cam.LookAt = options;
+    }
+
+    public void LookMainMenu() 
+    {
+        menu_Canvas.active = true;
+        options_Canvas.active = false;
+        credits_Canvas.active = false;
+        cam.Follow = mainMenu;
+        cam.LookAt = mainMenu;
+    }
+
+    public void LookCredits()
+    {
+        menu_Canvas.active = false;
+        options_Canvas.active = false;
+        credits_Canvas.active = true;
+        cam.Follow = credits;
+        cam.LookAt = credits;
     }
 
     public void InstantiateWindow(GameObject windowPrefab)
