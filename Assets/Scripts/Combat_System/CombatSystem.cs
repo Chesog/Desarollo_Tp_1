79e97ca459ce72b7638b973ee8b2dcd7d89f34f5
Dario <otrantodario@gmail.com>
2024-03-03 05:16:43 -0300

    Change Animation Speed

--- Assets/Scripts/Combat_System/CombatSystem.cs
+++ Assets/Scripts/Combat_System/CombatSystem.cs
@@ -29,13 +29,16 @@ public class CombatSystem : MonoBehaviour
 
     private void OnPlayerAttack(bool obj)
     {
-        Attack();
+        isAttacking = true;
     }
 
 
      //Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Fire1"))
+            Attack();
+    
         ExitAttack();
     }
 
