934d3db8fe62c790c0977924940fd2fbb79cf49e
Dario <otrantodario@gmail.com>
2024-03-04 07:21:30 -0300

    add new fsm logic

---
+++ Assets/Scripts/NewFSM/JumpState.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class JumpState : BaseState
+{
+    protected JumpState(PlayerController player, Animator animator) : base(player, animator) { }
+
+    public override void OnEnter()
+    {
+        _animator.CrossFade(JumpHash,crossFadeDuration);
+    }
+
+    public override void FixedUpdate()
+    {
+       //call for player jump logic and move logic
+    }
+}
---
+++ Assets/Scripts/NewFSM/LocomotionState.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class LocomotionState : BaseState
+{
+    protected LocomotionState(PlayerController player, Animator animator) : base(player, animator) { }
+
+    public override void OnEnter()
+    {
+        _animator.CrossFade(LocomotionHash,crossFadeDuration);
+    }
+    public override void FixedUpdate()
+    {
+         // call player controller move Logic
+    }
+}
